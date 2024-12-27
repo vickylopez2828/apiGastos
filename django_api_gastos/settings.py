9d0314f1315806aed8739b1b0dbee61e7f6ba122
vlopez2828 <vickylopez2828@gmail.com>
2024-12-27 10:38:03 -0300

    deploy vercel

--- django_api_gastos/settings.py
+++ django_api_gastos/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -81,8 +82,8 @@ TEMPLATES = [
         },
     },
 ]
-# WSGI_APPLICATION = 'django_api_gastos.wsgi.application'
-WSGI_APPLICATION = 'api.wsgi.app'
+WSGI_APPLICATION = 'django_api_gastos.wsgi.application'
+# WSGI_APPLICATION = 'api.wsgi.app'
 
 
 # Database
